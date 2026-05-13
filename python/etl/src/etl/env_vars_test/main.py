950c83fa170af12091085cae4f820b73ae0822fc
PriceGr <greg.price@unipartrail.com>
2026-05-13 14:42:49 +0100

    aligned python folder structure to best practice (will allow move env vars to be injected via .env for local dev or via secrets manager in cloud / container

---
+++ python/etl/src/etl/env_vars_test/main.py
@@ -0,0 +1,10 @@
+from shared.config.settings import get_settings
+
+settings = get_settings()
+
+print(settings)
+
+
+print(settings.env)
+
+print(1)
---
+++ python/machine_learning/src/machine_learning/env_vars_test/main.py
@@ -0,0 +1,9 @@
+from shared.config.settings import settings
+
+print(settings.param1)
+
+
+print(settings.param2)
+
+
+print(settings.env)
